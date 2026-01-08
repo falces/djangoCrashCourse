e4f60590d3f049e48a22e395f2f63baec85a95e0
falces <falces@gmail.com>
2026-01-08 10:46:08 +0100

    ADD: PostgreSQL

--- tutorialproject/core/settings.py
+++ tutorialproject/core/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgredev',
+        'USER': 'user',
+        'PASSWORD': 'reus',
+        'HOST': 'localhost',
+        'PORT': '15432',
     }
 }
 
